fe2ba18f9eb6478d0b5f3d53c4d45758b7821e0d
Sunil <sunil.6318ie@gmail.com>
2018-09-08 19:49:56 +0530

    UI updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatDatepickerModule, MatFormFieldModule, MatInputModule, MatNativeDateModule, MatSelectModule, MatOptionModule} from '@angular/material'
+
 import { AppComponent } from './app.component';
 import { BookinHomeComponent } from './bookin-home/bookin-home.component';
 
@@ -10,7 +13,15 @@ import { BookinHomeComponent } from './bookin-home/bookin-home.component';
     BookinHomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    MatOptionModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/bookin-home/bookin-home.component.ts
+++ src/app/bookin-home/bookin-home.component.ts
@@ -5,7 +5,27 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './bookin-home.component.html',
   styleUrls: ['./bookin-home.component.css']
 })
+
 export class BookinHomeComponent implements OnInit {
+
+  adults: any[] = [
+    {value: '1', viewValue: '1 Adult'},
+    {value: '2', viewValue: '2 Adults'},
+    {value: '3', viewValue: '3 Adults'},
+    {value: '4', viewValue: '4 Adults'},
+    {value: '5', viewValue: '5 Adults'},
+    {value: '6', viewValue: '6 Adults'},
+    {value: '7', viewValue: '7 Adults'},
+    {value: '8', viewValue: '8 Adults'}
+  ];
+
+  childrens: any[] = [
+    {value: '1', viewValue: '1 Child'},
+    {value: '2', viewValue: '2 Childrens'},
+    {value: '3', viewValue: '3 Childrens'},
+    {value: '4', viewValue: '4 Childrens'},
+    {value: '5', viewValue: '5 Childrens'},
+  ];
 
   constructor() { }
 
